5e5d206fbccae1869e901df35fb36590b73f6de8
mbehrozi1991-dot <mbehrozi1991@gmail.com>
2026-08-04 19:13:37 +0330

    Refactor app to use MDApp and reorganize screens

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
 
 from database import Database
 
-
-db = Database()
-
-
-class HomeScreen(Screen):
-    pass
-
-
-class ProductScreen(Screen):
-    pass
+from screens.home import HomeScreen
+from screens.products import ProductScreen
+from screens.customers import CustomerScreen
+from screens.sales import SaleScreen
+from screens.reports import ReportScreen
+from screens.expenses import ExpenseScreen
 
 
-class CustomerScreen(Screen):
-    pass
-
-
-class SaleScreen(Screen):
-    pass
-
-
-class ReportScreen(Screen):
-    pass
-
-
-
-class HesabyarApp(App):
+class HesabyarApp(MDApp):
 
     def build(self):
+
+        self.title = "حساب‌یار"
+
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.theme_style = "Light"
+
+        self.db = Database()
 
         Builder.load_file("hesabyar.kv")
 
@@ -42,12 +32,10 @@ class HesabyarApp(App):
         sm.add_widget(CustomerScreen(name="customers"))
         sm.add_widget(SaleScreen(name="sales"))
         sm.add_widget(ReportScreen(name="reports"))
+        sm.add_widget(ExpenseScreen(name="expenses"))
 
         return sm
 
 
-
 if __name__ == "__main__":
     HesabyarApp().run()
-# update
-# test 36
