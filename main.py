b0f1d1c8e7e4654f6cf33c1df2c2d4a1631348fa
mbehrozi1991-dot <mbehrozi1991@gmail.com>
2026-08-05 05:49:28 +0330

    Update main.py - keep existing structure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.navigationdrawer import MDNavigationDrawer
+from kivymd.uix.list import MDList, OneLineListItem
 
 from database import Database
 
